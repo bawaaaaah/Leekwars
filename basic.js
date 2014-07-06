05369dda29301323df09410aee2db40cef725b6e
Thiryn <dorianjolivald@gmail.com>
2014-07-06 19:03:03 +0200

    push de l IA de base Ameliorée

---
+++ basic.js
@@ -0,0 +1,17 @@
+//--------------------------------
+//------- Code de base -----------
+//--------------------------------
+
+// On prend le pistolet
+if (getWeapon() == -1)
+	setWeapon(WEAPON_PISTOL); // Attention : coûte 1 PT
+
+// On récupère l'ennemi le plus proche
+var enemy = getNearestEnemy();
+
+// On avance vers l'ennemi
+moveToward(enemy);
+
+// On essaye de lui tirer dessus
+useWeapon(enemy);
+useWeapon(enemy);
